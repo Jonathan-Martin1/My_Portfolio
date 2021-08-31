b1066d9b74dcc2377ad3a327d9906446ca97a1fa
Jonathan Martin <oldschoolspaceempire@gmail.com>
2021-08-31 16:11:02 -0500

    navbar

--- new-porfolio/src/App.js
+++ new-porfolio/src/App.js
@@ -1,12 +1,58 @@
 import React from 'react';
 
-import { MDBContainer } from 'mdb-react-ui-kit';
+import {
+  MDBContainer,
+  MDBIcon,
+  MDBNavbar,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBNavbarNav,
+  MDBNavbarToggler,
+} from 'mdb-react-ui-kit';
 
 function App() {
 	return (
 		<MDBContainer fluid>
 			<div class='row'>
-				<div class='col-md-8'>.col-md-8</div>
+				<div class='col-md-8'>
+					<header>
+						<MDBNavbar expand='lg' light bgColor='white'>
+							<MDBContainer fluid>
+								<MDBNavbarToggler
+									aria-controls='navbarExample01'
+									aria-expanded='false'
+									aria-label='Toggle navigation'>
+									<MDBIcon fas icon='bars' />
+								</MDBNavbarToggler>
+								<div className='collapse navbar-collapse' id='navbarExample01'>
+									<MDBNavbarNav right className='mb-2 mb-lg-0'>
+										<MDBNavbarItem active>
+											<MDBNavbarLink aria-current='page' href='#'>
+												Home
+											</MDBNavbarLink>
+										</MDBNavbarItem>
+										<MDBNavbarItem>
+											<MDBNavbarLink href='#'>Features</MDBNavbarLink>
+										</MDBNavbarItem>
+										<MDBNavbarItem>
+											<MDBNavbarLink href='#'>Pricing</MDBNavbarLink>
+										</MDBNavbarItem>
+										<MDBNavbarItem>
+											<MDBNavbarLink href='#'>About</MDBNavbarLink>
+										</MDBNavbarItem>
+									</MDBNavbarNav>
+								</div>
+							</MDBContainer>
+						</MDBNavbar>
+						<div className='p-5 text-center bg-light'>
+							<h1 className='mb-3'>Heading</h1>
+							<h4 className='mb-3'>Subheading</h4>
+							<a className='btn btn-primary' href='' role='button'>
+								Call to action
+							</a>
+						</div>
+					</header>
+				</div>
 				<div class='col-6 col-md-4'>.col-6 .col-md-4</div>
 			</div>
 			<div class='row'>
