a51090f09957118c5653cc91a9ddfa806eb7d406
Jonathan Martin <oldschoolspaceempire@gmail.com>
2021-08-31 16:03:47 -0500

    installed and started with MDB with React.js

--- new-porfolio/src/App.js
+++ new-porfolio/src/App.js
@@ -1,7 +1,25 @@
-import './App.css';
+import React from 'react';
+
+import { MDBContainer } from 'mdb-react-ui-kit';
 
 function App() {
-	return <div className='App'>Hello World</div>;
+	return (
+		<MDBContainer fluid>
+			<div class='row'>
+				<div class='col-md-8'>.col-md-8</div>
+				<div class='col-6 col-md-4'>.col-6 .col-md-4</div>
+			</div>
+			<div class='row'>
+				<div class='col-6 col-md-4'>.col-6 .col-md-4</div>
+				<div class='col-6 col-md-4'>.col-6 .col-md-4</div>
+				<div class='col-6 col-md-4'>.col-6 .col-md-4</div>
+			</div>
+			<div class='row'>
+				<div class='col-6'>.col-6</div>
+				<div class='col-6'>.col-6</div>
+			</div>
+		</MDBContainer>
+	);
 }
 
 export default App;
